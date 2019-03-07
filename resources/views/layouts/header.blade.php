6fb4afc615890daf1fdfedea9afa1b5ec684f70b
boryeira <jmanuel.jorquera@gmail.com>
2019-03-07 18:40:51 -0300

    template add

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,147 @@
+<header class="header">
+            <!-- START TOP-LEFT TOOLBAR-->
+            <ul class="nav navbar-toolbar">
+                <li>
+                    <a class="nav-link sidebar-toggler js-sidebar-toggler" href="javascript:;">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                </li>
+                <li class="dropdown mega-menu">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:;" aria-expanded="false">Mega</a>
+                    <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 51px, 0px); top: 0px; left: 0px; will-change: transform;">
+                        <div class="dropdown-arrow"></div>
+                        <div class="mega-toolbar-menu">
+                            <div class="d-flex">
+                                <a class="mega-toolbar-item" href="javascript:;">
+                                    <div class="item-icon"><i class="ti-file"></i></div>
+                                    <div class="item-name">Reports</div>
+                                    <div class="item-text">Lorem Ipsum is simply dummy.</div>
+                                    <div class="item-details">74 New</div>
+                                </a>
+                                <a class="mega-toolbar-item" href="javascript:;">
+                                    <div class="item-icon"><i class="ti-shopping-cart-full"></i></div>
+                                    <div class="item-name">Orders</div>
+                                    <div class="item-text">Lorem Ipsum is simply dummy.</div>
+                                    <div class="item-details">125 New</div>
+                                </a>
+                            </div>
+                            <div class="d-flex">
+                                <a class="mega-toolbar-item" href="javascript:;">
+                                    <div class="item-icon"><i class="ti-wallet"></i></div>
+                                    <div class="item-name">Profit</div>
+                                    <div class="item-text">Lorem Ipsum is simply dummy.</div>
+                                    <div class="item-details">+1200<sup>$</sup></div>
+                                </a>
+                                <a class="mega-toolbar-item" href="javascript:;">
+                                    <div class="item-icon"><i class="ti-support"></i></div>
+                                    <div class="item-name">Support</div>
+                                    <div class="item-text">Lorem Ipsum is simply dummy.</div>
+                                    <div class="item-details">54 New Ticket</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a class="nav-link search-toggler js-search-toggler"><i class="ti-search"></i>
+                        <span>Search here...</span>
+                    </a>
+                </li>
+            </ul>
+            <!-- END TOP-LEFT TOOLBAR-->
+            <!--LOGO-->
+            <a class="page-brand" href="index.html">A</a>
+            <!-- START TOP-RIGHT TOOLBAR-->
+            <ul class="nav navbar-toolbar">
+                <li class="dropdown dropdown-user">
+                    <a class="nav-link dropdown-toggle link" data-toggle="dropdown">
+                        <img src="./assets/img/users/admin-image.png" alt="image">
+                        <span>Super User</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-arrow dropdown-menu-right admin-dropdown-menu">
+                        <div class="dropdown-arrow"></div>
+                        <div class="dropdown-header">
+                            <div class="admin-avatar">
+                                <img src="./assets/img/users/admin-image.png" alt="image">
+                            </div>
+                            <div>
+                                <h5 class="font-strong text-white">Super User</h5>
+                                <div>
+                                    <span class="admin-badge mr-3"><i class="ti-alarm-clock mr-2"></i>30m.</span>
+                                    <span class="admin-badge"><i class="ti-lock mr-2"></i>Safe Mode</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="admin-menu-features">
+                            <a class="admin-features-item" href="javascript:;"><i class="ti-user"></i>
+                                <span>PROFILE</span>
+                            </a>
+                            <a class="admin-features-item" href="javascript:;"><i class="ti-support"></i>
+                                <span>SUPPORT</span>
+                            </a>
+                            <a class="admin-features-item" href="javascript:;"><i class="ti-settings"></i>
+                                <span>SETTINGS</span>
+                            </a>
+                        </div>
+                        <div class="admin-menu-content">
+                            <div class="text-muted mb-2">Your Wallet</div>
+                            <div><i class="ti-wallet h1 mr-3 text-light"></i>
+                                <span class="h1 text-success"><sup>$</sup>12.7k</span>
+                            </div>
+                            <div class="d-flex justify-content-between mt-2">
+                                <a class="text-muted" href="javascript:;">Earnings history</a>
+                                <a class="d-flex align-items-center" href="javascript:;">Logout<i class="ti-shift-right ml-2 font-20"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeout-toggler">
+                    <a class="nav-link toolbar-icon" data-toggle="modal" data-target="#session-dialog" href="javascript:;"><i class="ti-alarm-clock timeout-toggler-icon rel"><span class="notify-signal"></span></i></a>
+                </li>
+                <li class="dropdown dropdown-notification">
+                    <a class="nav-link dropdown-toggle toolbar-icon" data-toggle="dropdown" href="javascript:;"><i class="ti-bell rel"><span class="notify-signal"></span></i></a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-media">
+                        <div class="dropdown-arrow"></div>
+                        <div class="dropdown-header text-center">
+                            <div>
+                                <span class="font-18"><strong>14 New</strong> Notifications</span>
+                            </div>
+                            <a class="text-muted font-13" href="javascript:;">view all</a>
+                        </div>
+                        <div class="p-3">
+                            <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 320px;"><ul class="timeline scroller" data-height="320px" style="overflow: hidden; width: auto; height: 320px;">
+                                <li class="timeline-item"><i class="ti-check timeline-icon"></i>2 Issue fixed<small class="float-right text-muted ml-2 nowrap">Just now</small></li>
+                                <li class="timeline-item"><i class="ti-announcement timeline-icon"></i>
+                                    <span>7 new feedback
+                                        <span class="badge badge-warning badge-pill ml-2">important</span>
+                                    </span><small class="float-right text-muted">5 mins</small></li>
+                                <li class="timeline-item"><i class="ti-truck timeline-icon"></i>25 new orders sent<small class="float-right text-muted ml-2 nowrap">24 mins</small></li>
+                                <li class="timeline-item"><i class="ti-shopping-cart timeline-icon"></i>12 New orders<small class="float-right text-muted ml-2 nowrap">45 mins</small></li>
+                                <li class="timeline-item"><i class="ti-user timeline-icon"></i>18 new users registered<small class="float-right text-muted ml-2 nowrap">1 hrs</small></li>
+                                <li class="timeline-item"><i class="ti-harddrives timeline-icon"></i>
+                                    <span>Server Error
+                                        <span class="badge badge-success badge-pill ml-2">resolved</span>
+                                    </span><small class="float-right text-muted">2 hrs</small></li>
+                                <li class="timeline-item"><i class="ti-info-alt timeline-icon"></i>
+                                    <span>System Warning
+                                        <a class="text-purple ml-2">Check</a>
+                                    </span><small class="float-right text-muted ml-2 nowrap">12:07</small></li>
+                                <li class="timeline-item"><i class="fa fa-file-excel-o timeline-icon"></i>The invoice is ready<small class="float-right text-muted ml-2 nowrap">12:30</small></li>
+                                <li class="timeline-item"><i class="ti-shopping-cart timeline-icon"></i>5 New Orders<small class="float-right text-muted ml-2 nowrap">13:45</small></li>
+                                <li class="timeline-item"><i class="ti-arrow-circle-up timeline-icon"></i>Production server up<small class="float-right text-muted ml-2 nowrap">1 days ago</small></li>
+                                <li class="timeline-item"><i class="ti-harddrives timeline-icon"></i>Server overloaded 91%<small class="float-right text-muted ml-2 nowrap">2 days ago</small></li>
+                                <li class="timeline-item"><i class="ti-info-alt timeline-icon"></i>Server error<small class="float-right text-muted ml-2 nowrap">2 days ago</small></li>
+                            </ul><div class="slimScrollBar" style="background: rgb(113, 128, 143) none repeat scroll 0% 0%; width: 4px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px;"></div><div class="slimScrollRail" style="width: 4px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51) none repeat scroll 0% 0%; opacity: 0.9; z-index: 90; right: 1px;"></div></div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a class="nav-link quick-sidebar-toggler">
+                        <span class="ti-align-right"></span>
+                    </a>
+                </li>
+            </ul>
+            <!-- END TOP-RIGHT TOOLBAR-->
+        </header>
