c15f563a0066d9b26ee12fb4c95a55722ca6e9d7
boryeira <jmanuel.jorquera@gmail.com>
2019-04-24 14:08:22 -0400

    google verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="k9_GtS39bDe0uhv2mtzN7hp3jT7Jq_17lGwXs4zX0ws" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
