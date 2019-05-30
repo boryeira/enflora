397cd8294e24e2e7a3e1758e1ab247fd09aed7a2
boryeira <jmanuel.jorquera@gmail.com>
2019-05-30 12:54:53 -0400

    home

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -8,11 +8,18 @@
           <a href="/"><img src="{{asset('img/enflora.png')}}" width="450" style="margin-bottom:50px;margin-top:50px;"/></a>
           <p class="text-light">Club privado de cannabis medicinal</p>
 
+          <div class="card" style="margin:5px">
+            <a class="btn btn-primary" href="{{route('login')}}">Ingresar</a>
+            <br>
+            <a class="btn btn-info" href="{{route('form')}}">Formulario Pre-registro</a>
+
+          </div>
+
           <div class="ibox" style="margin:5px">
               <div class="ibox-body">
                   <ul class="timeline text-left">
-                      <li class="timeline-item"><i class="ti-desktop timeline-icon"></i> Sistema de suscripción y reserva en linea</li>
-                      <li class="timeline-item"><i class="ti-gallery timeline-icon"></i> Trazabilidad completa del cultivo</li>
+                      <li class="timeline-item"><i class="ti-desktop timeline-icon"></i>Plataforma de suscripción y reserva</li>
+                      <li class="timeline-item"><i class="ti-money timeline-icon"></i>Pago en linea</li>
                       <li class="timeline-item"><i class="ti-world timeline-icon"></i>Cultivo orgánico bajo estándares internacionales</li>
                       <li class="timeline-item"><i class="ti-headphone-alt timeline-icon"></i>Comunicación segura y siempre encriptada</li>
                       <li class="timeline-item"><i class="ti-lock timeline-icon"></i>Servicio discreto</li>
@@ -21,10 +28,27 @@
                   </ul>
               </div>
           </div>
+
+
           <div class="card" style="margin:5px">
-            <a class="btn btn-primary" href="{{route('login')}}">Ingresar</a>
-            <br>
-            <a class="btn btn-info" href="{{route('form')}}">Formulario Pre-registro</a>
+            <h5>Medios de pago:</h5>
+            <div class="row">
+              <div class="col-sm-4">
+                <img src="https://www.flow.cl/images/logos/webpay.png" class="logoWebpay" alt="logoWebpay">
+              </div>
+              <div class="col-sm-4">
+                <img src="https://www.flow.cl/images/logos/servipag.png" class="logoWebpay" alt="logoServipag">
+              </div>
+              <div class="col-sm-4">
+                <img src="https://www.flow.cl/images/medios-de-pago/onepay/onepay.png" class="logoWebpay" alt="logoOnepay">
+              </div>
+              <div class="col-sm-4">
+                <img src="https://www.flow.cl/images/logos/multicaja.png" class="logoWebpay" alt="logoMulticaja">
+              </div>
+              <div class="col-sm-4">
+                <img src="https://www.flow.cl/images/medios-de-pago/cryptocompra/cryptocompra.png" class="logoWebpay" alt="logoCryptocompra">
+              </div>
+            </div>
 
           </div>
           <div class="font-13" style="margin-bottom:50px;margin-top:50px;">2018 © Germinada por <b>Envolabs</b> - Cosechando datos</div>
