d8c16a5e8176b5f2a2540d64f9ff278e4d4a9783
boryeira <jmanuel.jorquera@gmail.com>
2019-06-06 01:06:18 -0400

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -105,7 +105,7 @@ class OrderController extends Controller
           $item->lote_id = $lote->id;
           $item->quantity = $q;
           $item->amount = $lote->price*$q;
-          $item->status = 1;
+          $item->status = 2;
           $item->save();
           $lote->consumed = $lote->consumed + $q;
           $lote->save();
