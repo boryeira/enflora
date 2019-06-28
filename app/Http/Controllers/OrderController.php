9725cb6b7f446dfea11efe8c697c9944a6bcd2ce
boryeira <jmanuel.jorquera@gmail.com>
2019-06-28 16:25:17 -0400

    nuevo

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -266,6 +266,38 @@ class OrderController extends Controller
 
     }
 
+    public function confirmFlow(Request $request)
+    {
+      
+      // $flow = Flow::make('sandbox', [
+      //         'apiKey'    => '367F3C6A-DEB8-46F7-89E5-32CLED2236B9',
+      //         'secret'    => '65d9f9656b478aaa7be72267bc33f40747f47c94',
+      //     ]);
+      $flow = Flow::make('production', [
+              'apiKey'    => '7B19A4CF-F041-40C4-9488-4180L75A6AAA',
+              'secret'    => '8a8c824cd4550b1ee4d581a1d3404d9d640638b0',
+          ]);
+
+      $payment = $flow->payment()->get($request->token);
+
+      $paymentData = $payment->paymentData;
+      $order = Order::find($payment->commerceOrder);
+
+      if($paymentData['date']==null){
+        return Redirect::route('orders.index')->withErrors(array('flow' =>'no se realizo el pago'));
+      } else {
+        if($order->status==2){
+          $order->status = 3;
+          $order->pay_date = $paymentData['date'];
+          $order->save();
+        }
+
+       
+        return 'ok';
+      }
+      return 'ok';
+
+    }
 
     // public function mail()
     // {
